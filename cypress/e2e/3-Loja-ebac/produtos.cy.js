f4b2572f9ee07a841f107525f954eb015693c208
Gabriel Urquiza <gabrielrodrigues2223@hotmail.com>
2024-10-16 22:12:09 -0300

    Teste de produtos

---
+++ cypress/e2e/3-Loja-ebac/produtos.cy.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress"/>
+
+beforeEach(() => {
+    cy.visit ('http://lojaebac.ebaconline.art.br/produtos/#')
+});
+
+describe('Funcionalidade: Produtos', () => {
+    
+    it('Deve selecionar um produto da lista', () => {
+        cy.get(' .product-block')
+            .first()
+            .click()
+            cy.get('#tab-title-additional_information > a').should('contain', 'Informação adicional')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get(' .product-block')
+            .last()
+            .click()
+            cy.get('#tab-title-additional_information > a').should('contain', 'Informação adicional')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get(' .product-block')
+            .eq(2)
+            .click()
+            cy.get('#tab-title-additional_information > a').should('contain', 'Informação adicional')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get(' .product-block')
+            .contains('Aero Daily Fitness Tee')
+            .click()
+            cy.get('#tab-title-additional_information > a').should('contain', 'Informação adicional')
+    });
+
+});
