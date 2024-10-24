dde042b4276608c23b456681c72af5aeb4decceb
Gabriel Urquiza <gabrielrodrigues2223@hotmail.com>
2024-10-23 23:41:53 -0300

    unsando massa de dados e Fixture

--- cypress/e2e/3-Loja-ebac/login.cy.js
+++ cypress/e2e/3-Loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
     afterEach(() => {
         cy.screenshot()
@@ -19,7 +20,7 @@ it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
     cy.get('#username').type('gabrielrodrigues@hotmail.com')
     cy.get('#password').type('030599gu@')
     cy.get('.woocommerce-form > .button').click()  
-    cy.get('.woocommerce-error').should ('contain', 'Endereço de e-mail desconhecido.')
+    cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido.')
     cy.get('.woocommerce-error').should('exist')
     
 });
@@ -28,8 +29,26 @@ it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
     cy.get('#username').type('gabrielrodrigues2223@hotmail.com')
     cy.get('#password').type('030599gu')
     cy.get('.woocommerce-form > .button').click()   
-    cy.get('.woocommerce-error').should ('contain', 'Erro: A senha fornecida para o e-mail gabrielrodrigues2223@hotmail.com está incorreta. Perdeu a senha?')
+    cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail gabrielrodrigues2223@hotmail.com está incorreta. Perdeu a senha?')
     cy.get('.woocommerce-error').should('exist')
+});
+
+it('Deve fazer login com sucesso - usando massa de dados', () => {
+    cy.get('#username').type(perfil.usuario)
+    cy.get('#password').type(perfil.senha)
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gabrielrodrigues2223')
+});
+
+it.only('Deve fazer login com sucesso - usando Fixture', () => {
+
+cy.fixture('perfil').then(dados => {
+    cy.get('#username').type(dados.usuario , {log: false})
+    cy.get('#password').type(dados.senha , {log: false})
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gabrielrodrigues2223')   
+})
+
 });
 
 })
